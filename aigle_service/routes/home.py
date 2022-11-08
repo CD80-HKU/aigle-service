7966e8629869c6ffa06a3c76b952a876aeb68a06
CallanBi <mcbihv@126.com>
2022-11-08 12:09:48 +0800

    feat: add test

--- aigle_service/routes/home.py
+++ aigle_service/routes/home.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 home = Blueprint('home', __name__)
 
 
-@home.route('/datasource/list', methods=['GET'])
+@home.route('/api/datasource/list', methods=['GET'])
 def index():
     return {
         'data': 'Hello World!',
